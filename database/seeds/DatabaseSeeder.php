a40e133d3f505f219a79c17ae77efe228e2514b3
Vento-Nuenenen <vento@pfadi-nuenenen.ch>
2020-08-28 19:44:36 +0200

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(GroupsTableSeeder::class);
-        $this->call(RolesTableSeeder::class);
-        $this->call(UsersTableSeeder::class);
+        // $this->call(UserSeeder::class);
     }
 }
