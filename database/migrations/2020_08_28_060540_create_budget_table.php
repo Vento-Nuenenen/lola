da023b35c0a542aead91e175e3cf9e6ed2dadb23
Vento-Nuenenen <vento@pfadi-nuenenen.ch>
2020-08-29 10:24:42 +0200

    Update 2020_08_28_060540_create_budget_table.php

--- database/migrations/2020_08_28_060540_create_budget_table.php
+++ database/migrations/2020_08_28_060540_create_budget_table.php
@@ -15,6 +15,7 @@ class CreateBudgetTable extends Migration
     {
         Schema::create('budget', function (Blueprint $table) {
             $table->id();
+            $table->string();
             $table->timestamps();
         });
     }
