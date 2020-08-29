6774394658e63ec7940da6cf62e6b2fba2c65e1d
Vento-Nuenenen <vento@pfadi-nuenenen.ch>
2020-08-29 10:24:33 +0200

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->foreignId('group_id');
             $table->rememberToken();
             $table->timestamps();
         });
